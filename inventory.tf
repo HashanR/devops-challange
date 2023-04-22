477bf499dc3474b0a7215229cb02afadb5e32357
HashanR <rmhashan@gmail.com>
2023-04-22 14:35:14 +0530

    Ansible inventory generation using template

--- inventory.tf
+++ inventory.tf
@@ -3,6 +3,6 @@ resource "local_file" "ansible_inventory" {
   depends_on = [data.template_file.ansible_skeleton]
 
   content  = data.template_file.ansible_skeleton.rendered
-  filename = "${path.root}/inventory"
+  filename = "${path.root}/ansible/inventory"
 }
 
